151b8b98d0e68769821a46b73c3ca979b910100d
FajriRamadhan30 <fajrirmdn03@gmail.com>
2025-06-08 20:08:19 +0700

    vrf success

--- backend/routes/vlanVrfRoutes.js
+++ backend/routes/vlanVrfRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const controller = require('../controllers/vlanVrfController');
 
 // Route untuk VLAN dan VRF
-router.get('/', controller.getAllVLANs);  // Mengambil semua VLAN & VRF
+router.get('/', controller.getAllData); // Mengambil semua VLAN & VRF
 router.post('/vlan', controller.createVLAN);  // Menambah VLAN
 router.post('/vrf', controller.createVRF);  // Menambah VRF
 
